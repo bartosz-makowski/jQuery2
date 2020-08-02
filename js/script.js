901ddee9fc7d1960024eecab820c795943047471
Bartek_makowski <mako19895@gmail.com>
2020-08-02 20:04:46 +0000

    removed table.html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
-    $("h2").addClass("underline");
-    $("ul").addClass("border");
-});
+    $("#stream1_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+}); 
